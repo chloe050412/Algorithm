99ea199f36236a892b09d1abf37cdf4fa72234e4
chloe050412 <92531179+chloe050412@users.noreply.github.com>
2023-11-07 01:17:00 +0900

    1845.Seat Reservation Manager

---
+++ LEETCODE/1845.Seat Reservation Manager.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+
+class SeatManager
+{
+public:
+    SeatManager(int n)
+    {
+        _seats.resize(n);
+        _index = 1;
+    }
+
+    int reserve()
+    {
+        if (_canceled.empty())
+        {
+            _seats[_index - 1] = 1;
+            ++_index;
+            return _index - 1;
+        }
+        else
+        {
+            int min = 500000;
+            int index = 0;
+            for (int i = 0; i < _canceled.size(); ++i)
+            {
+                if (_canceled[i] < min)
+                {
+                    min = _canceled[i];
+                    index = i;
+                }
+            }
+
+            _canceled.erase(_canceled.begin() + index);
+            return min;
+        }
+    }
+
+    void unreserve(int seatNumber)
+    {
+        _seats[seatNumber - 1] = 0;
+        _canceled.push_back(seatNumber);
+    }
+
+private:
+    std::vector<int> _seats;
+    std::vector<int> _canceled;
+    int _index;
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
+
+int main()
+{
+    SeatManager* man = new SeatManager(5);
+    int a1 = man->reserve();
+    int a2 = man->reserve();
+    man->unreserve(2);
+    int a3 = man->reserve();
+    int a4 = man->reserve();
+    int a5 = man->reserve();
+    int a6 = man->reserve();
+    man->unreserve(5);
+
+
+    return 0;
+}
